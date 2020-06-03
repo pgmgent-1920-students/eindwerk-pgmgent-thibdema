e1b77a9e64cba66b1b008667c75a9bb6e2708a8f
Codelicious <thibdema@student.arteveldehs.be>
2020-06-03 13:19:36 +0200

    Working with firebase functions

---
+++ functions/functions/src/index.ts
@@ -0,0 +1,13 @@
+import * as functions from 'firebase-functions';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.scheduledFunction = functions.pubsub.schedule('every 5 minutes').onRun((context) => {
+    console.log('This will be run every 5 minutes!');
+    return null;
+  });
